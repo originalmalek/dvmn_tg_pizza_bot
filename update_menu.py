f87b0acc59d3f4e0988c436bbf609b8f5069344d
originalmalek <originalmalek@gmail.com>
2022-12-29 13:48:36 +0100

    Delete excess code

--- update_menu.py
+++ update_menu.py
@@ -1,24 +1,10 @@
-import redis
-import requests
-
 from dotenv import load_dotenv
-import os
+
+from main import get_database_connection
 from motlin_api import get_products
-from datetime import datetime
-_database = None
-
-def get_database_connection():
-    global _database
-    if _database is None:
-        database_password = os.getenv('DATABASE_PASSWORD')
-        database_host = os.getenv('DATABASE_HOST')
-        database_port = os.getenv('DATABASE_PORT')
-        _database = redis.Redis(host=database_host, port=database_port, password=database_password)
-    return _database
 
 
-def upload_menu_to_redis():
-    menu = get_products()
+def upload_menu_to_redis(menu):
     db.set('menu', str(menu))
 
 
@@ -26,9 +12,5 @@ if __name__ == '__main__':
     load_dotenv()
     db = get_database_connection()
 
-    cached_menu = db.get('menu')
-    time_diff = datetime.now() - cached_menu['created_at']
-    if time_diff.hours > 0:
-        upload_menu_to_redis()
-    else:
-        menu = cached_menu
+    menu = get_products()
+    upload_menu_to_redis(menu)
